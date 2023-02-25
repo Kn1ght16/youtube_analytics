edb83423cbd98546de8f9f537c96ae4457be3ea2
Kn1ght16 <Danchenko0@mail.ru>
2023-02-25 20:54:57 +0300

    task1 13.2 add to json

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ def main():
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     vdud.print_info()
 
+    # получаем значения атрибутов
+    print(vdud.title)
+    print(vdud.video_count)
+    print(vdud.url)
+
+    # менять не можем
+    vdud.__id= 'Новое название'
+
+    # можем получить объект для работы с API вне класса
+    print(vdud.get_service())
+
+    # создать файл 'vdud.json' в данными по каналу
+    vdud.to_json('vdud.json')
+
 
 if __name__ == "__main__":
     main()
--- tests/channel_test.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from channel import Channel
-
-
-#def test_get_json():
